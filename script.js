d630eb636e3980efba19344fca52a484948ec7f5
Statikman619 <martinez.michael02@gmail.com>
2021-03-30 21:50:37 -0700

    worked on JS

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 var startButton = document.getElementById("start-button");
 var timer = document.getElementById("timer");
+var questionContainerElement = document.getElementById("questionContainerElement")
+var questionElement = document.getElementById("question")
+var answerButtonsElement = document.getElementById("answerButtons")
 var timeLeft = 75
 
-startButton.addEventListener("click", function(){
-
-}
+startButton.addEventListener("click", startQuiz)
 
 var timerInterval = setInterval(function() {
     timeLeft--;
     timer.textContent = "Time Left: " + timeLeft;
     if (timeLeft <=0)
     }, 1000)
+}
+
+function startQuiz(){
+    console.log("started")
+    startButton.classList.add("hide")
+    questionContainerElement.classList.remove("hide")
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionsIndex = 0
+    questionContainerElement.classList.remove("hide")
+    setNextQuestion()
+}
+
+function setNextQuestion(){
+    showQuestion(shuffledQuestions[currentQuestionsIndex]);
+}
+
+function showQuestion(question) {
+    
+}
+
+function selectAnswer(){
 
 }
 
