cf8a4d513510480f67d32571cf40d3531b9ebbd3
Statikman619 <martinez.michael02@gmail.com>
2021-03-30 21:12:15 -0700

    started some work

---
+++ script.js
@@ -0,0 +1,17 @@
+var startButton = document.getElementById("start-button");
+var timer = document.getElementById("timer");
+var timeLeft = 75
+
+startButton.addEventListener("click", function(){
+
+}
+
+var timerInterval = setInterval(function() {
+    timeLeft--;
+    timer.textContent = "Time Left: " + timeLeft;
+    if (timeLeft <=0)
+    }, 1000)
+
+}
+
+
