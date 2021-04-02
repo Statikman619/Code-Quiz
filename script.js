857cab2bb98ca97dec263668fc36cffa042a6ee0
Statikman619 <martinez.michael02@gmail.com>
2021-04-01 20:15:26 -0700

    major HTML, CSS and JS changes

--- script.js
+++ script.js
@@ -1,167 +1,178 @@
+const questions = [
+  {
+    question: "When was Javascript created?",
+    answers: [
+      { text: "1995", correct: true },
+      { text: "1993", correct: false },
+      { text: "1997", correct: false },
+      { text: "1999", correct: false },
+    ],
+  },
+  {
+    question: "What are Javascript data types?",
+    answers: [
+      { text: "Number", correct: true },
+      { text: "String", correct: true },
+      { text: "Boolean", correct: true },
+      { text: "All the above", correct: true },
+    ],
+  },
+  {
+    question: "Is Javascript case sensitive?",
+    answers: [
+      { text: "Yes", correct: true },
+      { text: "No", correct: false },
+      { text: "Maybe", correct: false },
+      { text: "I don't know", correct: false },
+    ],
+  },
+  {
+    question: "What does DOM stand for?",
+    answers: [
+      { text: "Does only math", correct: false },
+      { text: "Document Object Model", correct: true },
+      { text: "Daily objective media", correct: false },
+      { text: "Document overview media", correct: false },
+    ],
+  },
+  {
+    question: "Which built in method sorts the elements of an array?",
+    answers: [
+      { text: "Reverse", correct: false },
+      { text: "Element", correct: false },
+      { text: "Sort", correct: true },
+      { text: "Object", correct: false },
+    ],
+  },
+];
+
 var startButton = document.getElementById("start-button");
-var nextButton = document.getElementById("next-button")
+var nextButton = document.getElementById("next-button");
 var timer = document.getElementById("timer");
-var questionContainerElement = document.getElementById("question-container")
-var questionElement = document.getElementById("question")
-var answerButtonsElement = document.getElementById("answer-buttons")
-var timeLeft = 75
+var questionContainerElement = document.getElementById("question-container");
+var questionElement = document.getElementById("question");
+var answerButtonsElement = document.getElementById("answer-buttons");
+var timeLeft = 75;
+// var quizIndex = 0
+let timerInterval;
+let shuffledQuestions, quizIndex;
 
-let shuffledQuestions, currentQuestionsIndex
-
-startButton.addEventListener("click", startQuiz)
+startButton.addEventListener("click", startQuiz);
 nextButton.addEventListener("click", () => {
-    currentQuestionsIndex++
-    setNextQuestion()})
+  quizIndex++;
+  setNextQuestion();
+});
 
-function startTimer(){
-    var timerInterval = setInterval(function() {
-        timeLeft--;
-        timer.textContent = "Time Left: " + timeLeft;
-        if (timeLeft <=0){
-            clearInterval(timerInterval)
-        }
-        }, 1000)
+function startTimer() {
+  timerInterval = setInterval(function () {
+    timeLeft--;
+    timer.textContent = "Time Left: " + timeLeft;
+    if (timeLeft <= 0) {
+      clearInterval(timerInterval);
+      timeLeft = 0;
+      timer.textContent = "Time Left: " + timeLeft;
+    }
+  }, 1000);
 }
 
 var initials = "";
 var score = 0;
 var highScoreList = JSON.parse(localStorage.getItem("highscore")) || [];
-var list = JSON.parse("highscore")
+// var list = JSON.parse("highscore")
 
-SVGNumberList.addEventListener("click", function(){
-    highScoreList.name = initials;
-    highScoreList.score = score;
-    hideAll();
-    showScore();
+document.querySelector(".Highscores").addEventListener("click", function () {
+  highScoreList.name = initials;
+  highScoreList.score = score;
+  hideAll();
+  showScore();
+});
+
+function startQuiz() {
+  startTimer();
+  startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  quizIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  //hide submit button
+  setNextQuestion();
 }
 
-function startQuiz(){
-    startTimer()
-    startButton.classList.add("hide")
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionsIndex = 0
-    questionContainerElement.classList.remove("hide")
-    setNextQuestion()
-}
-
-function setNextQuestion(){
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionsIndex])
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[quizIndex]);
 }
 
 function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-        var button = document.createElement("button")
-        button.innerText = answer.text
-        button.classList.add("button")
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtonElement.appendChild(button)
-    })
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    var button = document.createElement("button");
+    button.textContent = answer.text;
+    button.classList.add("button");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    var answerButtonElement = document.getElementById("answer-buttons");
+    answerButtonElement.appendChild(button);
+  });
 }
 
 function resetState() {
-    clearStatusClass(document.body)
-    nextButton.classList.add("hide")
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild
-        (answerButtonsElement.firstChild)
-    }
+  clearStatusClass(document.body);
+  nextButton.classList.add("hide");
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
 }
 
-function selectAnswer(e){
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    if (shuffledQuestions.length > currentQuestionsIndex + 1){
-    nextButton.classList.remove("hide")
-    } else {
-        startButton.innerText = "restart"
-        startButton.classList.remove("hide")
-    }}
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  if (!correct) {
+    timeLeft = timeLeft - 10;
+    timer.textContent = "Time Left: " + timeLeft;
+  }
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (shuffledQuestions.length > quizIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    console.log(quizIndex);
+    startButton.innerText = "restart";
+    startButton.classList.remove("hide");
+    endGame();
+  }
+}
 
 function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct){
-        element.classList.add("correct")
-    }   else {
-        element.classList.add("wrong")
-    }
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
 }
 
 function clearStatusClass(element) {
-    element.classList.remove("correct")
-    element.classList.remove("wrong")
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
 }
 
-const questions = [
-    {
-    question: "When was Javascript created?",
-    answers: [
-        { Text: "1995", correct: true},
-        { Text: "1993", correct: false},
-        { Text: "1997", correct: false},
-        { Text: "1999", correct: false}
-       ]
-    },
-    { 
-    question: "What are Javascript data types?",
-    answers: [
-        { Text: "Number", correct: true},
-        { Text: "String", correct: true},
-        { Text: "Boolean", correct: true},
-        { Text: "All the above", correct: true}
-       ]
-    },
-    {
-    question: "Is Javascript case sensitive?",
-    answers: [
-        { Text: "Yes", correct: true},
-        { Text: "No", correct: false},
-        { Text: "Maybe", correct: false},
-        { Text: "I don't know", correct: false}
-       ]
-    }, 
-    {
-    question: "What does DOM stand for?",
-    answers: [
-        { Text: "Does only math", correct: false},
-        { Text: "Document Object Model", correct: true},
-        { Text: "Daily objective media", correct: false},
-        { Text: "Document overview media", correct: false}
-       ]
-    },
-    {
-    question: "Which built in method sorts the elements of an array?",
-    answers: [
-        { Text: "Reverse", correct: false},
-        { Text: "Element", correct: false},
-        { Text: "Sort", correct: true},
-        { Text: "Object", correct: false}
-       ]
-    }, 
-]
-
-    quizIndex++;
-    if (quizIndex > 4) {
-        //run end quiz function
-        clearInterval(timerInterval);
-        console.log("Quiz Over");
-    } else {
-        startQuiz();
-}
-document.querySelector("#initials-submit").addEventListener("click", function) {
-    initials + document.querySelector("#initials")
+document
+  .querySelector("#initials-submit")
+  .addEventListener("click", function () {
+    initials + document.querySelector("#initials");
     console.log(initials);
-})
+  });
 
 function endGame() {
-    //end the quiz
-    //track your score by logging your initials
+  clearInterval(timerInterval);
+  timeLeft = 0;
+  timer.textContent = "Time Left: " + timeLeft;
+  //hide all the buttons document ID and answer buttons
+  //
+  //end the quiz
+  //track your score by logging your initials
 }
